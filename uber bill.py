391756cf4e8355243279dec0c8dc7d6e5119dfe5
meera-mai <60419910+meera-mai@users.noreply.github.com>
2020-01-29 15:11:51 +0530

    Add files via upload

---
+++ uber bill.py
@@ -0,0 +1,13 @@
+def myfun(a,b):
+    if src=='avadi' and des=='ponamalle':
+        print("the amount is 50")
+    elif src=='avadi' and des=='redhills':
+        print("the amount is 80")
+    else:
+        print("check the enter location")
+
+
+
+src=raw_input("enter the src")
+des=raw_input("enter the des")
+myfun(src,des)
